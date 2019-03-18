91f052c6de62077f5f8f1f7a5f54ddce492e56a9
andrewbituin <fandrewbituin@gmail.com>
2019-03-18 09:40:48 -0700

    working on adding authRouter to app.js

---
+++ pref-card-api/auth/auth-service.js
@@ -0,0 +1,32 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("../config");
+
+const AuthService = {
+  parseBasicToken(bearerToken) {
+    return Buffer.from(bearerToken, "base64")
+      .toString()
+      .split(":");
+  },
+  getUserWithUserName(db, user_name) {
+    return db("prefcard_users")
+      .where({ user_name })
+      .first();
+  },
+  comparePasswords(password, hash) {
+    return bcrypt.compare(password, hash);
+  },
+  createJwt(subject, payload) {
+    return jwt.sign(payload, config.JWT_SECRET, {
+      subject,
+      algorithm: "HS256"
+    });
+  },
+  verifyJwt(token) {
+    return jwt.verify(token, config.JWT_SECRET, {
+      algorithms: ["HS256"]
+    });
+  }
+};
+
+module.exports = AuthService;
